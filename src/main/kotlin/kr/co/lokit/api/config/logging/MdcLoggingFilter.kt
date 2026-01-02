3e66408b07b4c19388f0e33617bbb11e293e25a9
JihwanByun <jihwaja9091@naver.com>
2026-01-02 16:15:08 +0900

    fix: filter 서블릿 내 수동 등록 변경

--- src/main/kotlin/kr/co/lokit/api/config/logging/MdcLoggingFilter.kt
+++ src/main/kotlin/kr/co/lokit/api/config/logging/MdcLoggingFilter.kt
@@ -5,17 +5,12 @@ import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.slf4j.LoggerFactory
 import org.slf4j.MDC
-import org.springframework.core.Ordered
-import org.springframework.core.annotation.Order
 import org.springframework.http.MediaType
-import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 import org.springframework.web.util.ContentCachingRequestWrapper
 import org.springframework.web.util.ContentCachingResponseWrapper
 import java.util.UUID
 
-@Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
 class MdcLoggingFilter : OncePerRequestFilter() {
     private val log = LoggerFactory.getLogger(javaClass)
 
