8f8b65f1b649cce7879469dfabff804d27388e76
김승진 <ohksj77@gmail.com>
2026-01-14 23:42:52 +0900

    setting: audit base entity 추가

---
+++ src/main/kotlin/kr/co/lokit/api/common/entity/BaseEntity.kt
@@ -0,0 +1,50 @@
+package kr.co.lokit.api.common.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.MappedSuperclass
+import org.hibernate.proxy.HibernateProxy
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+import java.util.*
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private var id: Long? = null
+
+    @CreatedDate
+    @Column(updatable = false)
+    var createdAt: LocalDateTime = LocalDateTime.now()
+
+    @LastModifiedDate
+    var updatedAt: LocalDateTime = LocalDateTime.now()
+
+    override fun equals(other: Any?): Boolean {
+        if (other == null) {
+            return false
+        }
+
+        if (other !is HibernateProxy && this::class != other::class) {
+            return false
+        }
+
+        return id == getIdentifier(other)
+    }
+
+    private fun getIdentifier(obj: Any): Long? =
+        if (obj is HibernateProxy) {
+            (obj.hibernateLazyInitializer.implementation as BaseEntity).id
+        } else {
+            (obj as BaseEntity).id
+        }
+
+    override fun hashCode() = Objects.hashCode(id)
+}
