952b717d11ae79c04ddf18e9b671206a88594484
JihwanByun <jihwaja9091@naver.com>
2026-01-20 21:06:54 +0900

    feat: null 처리로 변경

--- src/main/kotlin/kr/co/lokit/api/domain/user/infrastructure/UserRepository.kt
+++ src/main/kotlin/kr/co/lokit/api/domain/user/infrastructure/UserRepository.kt
@@ -1,10 +1,10 @@
 package kr.co.lokit.api.domain.user.infrastructure
 
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.Optional
+import org.springframework.security.core.userdetails.UserDetails
 
 interface UserRepository : JpaRepository<UserEntity, Long> {
-    fun findByEmail(email: String): Optional<UserEntity>
+    fun findByEmail(email: String): UserDetails?
 
     fun existsByEmail(email: String): Boolean
 }
