c5d81ef3e9c1cca6dc0a988d35a6dc6e77e6239e
김승진 <ohksj77@gmail.com>
2026-02-07 03:08:31 +0900

    chore: remove cors property

--- src/main/kotlin/kr/co/lokit/api/config/web/CorsProperties.kt
+++
@@ -1,8 +0,0 @@
-package kr.co.lokit.api.config.web
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-
-@ConfigurationProperties(prefix = "cors")
-data class CorsProperties(
-    val allowedOrigins: List<String> = emptyList(),
-)
